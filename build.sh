d131f944b89d02222fd384b432b0778c0fb1396a
Jmz <jmztaylor@gmail.com>
2012-08-31 17:58:30 -0700

    align to my standards

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "****Start building****"
+echo "****Cleaning****"
+
+make clean
+make mrproper
+rm output/kernel/zImage
+rm output/system/lib/modules/msm-buspm-dev.ko
+rm output/system/lib/modules/kineto_gan.ko
+rm output/system/lib/modules/bcm4329.ko
+rm output/system/lib/modules/sequans_sdio.ko
+rm output/system/lib/modules/spidev.ko
+rm *.zip
+
+START=$(date +%s)
+echo "****Building****"
+
+make shooter_defconfig
+make -j9
+
+echo "****Creating boot image****"
+cp arch/arm/boot/zImage output/kernel/zImage
+cp drivers/net/wimax/SQN/sequans_sdio.ko output/system/lib/modules/sequans_sdio.ko
+cp drivers/net/kineto_gan.ko output/system/lib/modules/kineto_gan.ko
+cp drivers/net/wireless/bcm4329_248/bcm4329.ko output/system/lib/modules/bcm4329.ko
+cp drivers/spi/spidev.ko output/system/lib/modules/spidev.ko
+cp arch/arm/mach-msm/msm-buspm-dev.ko output/system/lib/modules/msm-buspm-dev.ko
+cd output
+zip -q -r Jmz-Kernel-SHOOTER-$(date +"%Y%m%d").zip .
+
+echo "****Compile done****"
+echo "****Kernel and modules are in output/****"
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
+read -n 1 -p "Press any key to continue"
