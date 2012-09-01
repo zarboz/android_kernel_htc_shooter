4c2fc187d62b1002e58eebb5f7a457f85331817e
Jmz <jmztaylor@gmail.com>
2012-09-01 09:49:04 -0700

    fix htc_battery_core

--- arch/arm/mach-msm/htc_battery_core.c
+++ arch/arm/mach-msm/htc_battery_core.c
@@ -390,7 +390,7 @@ static int htc_battery_create_attrs(struct device *dev)
 
 htc_attrs_failed:
 	while (i--)
-		device_remove_file(dev, &htc_battery_attrs[i]);
+		device_remove_file(dev, &htc_battery_attrs[j]);
 htc_delta_attrs_failed:
 	while (j--)
 		device_remove_file(dev, &htc_set_delta_attrs[i]);
