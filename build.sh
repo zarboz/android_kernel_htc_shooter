452fee06637cdbc4071f050cb970673f13f683b9
Jmz <jmztaylor@gmail.com>
2012-09-08 15:07:37 -0700

    Revert "fix script"
    This reverts commit 710f4977ad61211f467fc6f0f6d9f398986e0602.

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ make mrproper
 rm output/kernel/zImage
 rm output/system/lib/modules/msm-buspm-dev.ko
 rm output/system/lib/modules/kineto_gan.ko
-rm output/system/lib/modules/bcmdhd.ko
+rm output/system/lib/modules/bcm4329.ko
 rm output/system/lib/modules/sequans_sdio.ko
+rm output/system/lib/modules/spidev.ko
 rm output/*.zip
 
 START=$(date +%s)
@@ -25,7 +26,8 @@ mkdir output/system/lib/modules
 cp arch/arm/boot/zImage output/kernel/zImage
 cp drivers/net/wimax/SQN/sequans_sdio.ko output/system/lib/modules/sequans_sdio.ko
 cp drivers/net/kineto_gan.ko output/system/lib/modules/kineto_gan.ko
-cp drivers/net/wireless/bcmdhd/bcmdhd.ko output/system/lib/modules/bcmdhd.ko
+cp drivers/net/wireless/bcm4329_248/bcm4329.ko output/system/lib/modules/bcm4329.ko
+cp drivers/spi/spidev.ko output/system/lib/modules/spidev.ko
 cp arch/arm/mach-msm/msm-buspm-dev.ko output/system/lib/modules/msm-buspm-dev.ko
 cd output
 zip -q -r Jmz-Kernel-SHOOTER-$(date +"%Y%m%d").zip .
