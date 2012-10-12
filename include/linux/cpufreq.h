8956023acb9e8fc2a717606cfafb92f5a33b4af9
showp1984 <showp1984@gmail.com>
2012-10-12 14:45:46 -0700

    Badass Governor (Support Both of 2 and 3 Phases)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -30,6 +30,11 @@ extern uint32_t acpu_check_khz_value(unsigned long khz);
 ssize_t set_scaling_max(unsigned int new_max, int cpu);
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
+/* Badass gpu state detection */
+extern bool gpu_busy_state;
+#endif
+
 /*********************************************************************
  *                     CPUFREQ NOTIFIER INTERFACE                    *
  *********************************************************************/
@@ -392,6 +397,9 @@ extern struct cpufreq_governor cpufreq_gov_savagedzen;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
 extern struct cpufreq_governor cpufreq_gov_scary;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_scary)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
 #endif
 
 
